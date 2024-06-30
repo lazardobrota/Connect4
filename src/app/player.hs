cb53e55069b4f831448a123bc88ee8d4b60182a3
Lazar Dobrota <lazardobrota1323@gmail.com>
2024-06-30 23:18:57 +0200

    choose color for specific player

--- src/app/player.hs
+++ src/app/player.hs
@@ -1,6 +1,15 @@
+import Board
+
+
+
+
 data Turn = Player1 | Player2 deriving Eq
 
 
 switchTurn :: Turn -> Turn
 switchTurn Player1 = Player2
 switchTurn Player2 = Player1
+
+playerPiece :: Turn -> Piece
+playerPiece Player1 = Red
+playerPiece Player2 = Yellow
