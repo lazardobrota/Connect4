a3ebb42c9654cc84fd398e24069207c87780bd7d
Lazar Dobrota <lazardobrota1323@gmail.com>
2024-07-03 00:16:57 +0200

    add ability to chain moves with state monad

--- src/app/player.hs
+++ src/app/player.hs
@@ -1,4 +1,6 @@
-module Player 
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Use if" #-}
+module Player
 ( switchTurn
 , playerPiece
 ) where
@@ -6,8 +8,6 @@ module Player
 import Board
 
 data Turn = Player1 | Player2 deriving Eq
-
-data Status b a = Wrong b | Correct a deriving Eq --same as Either but different Show implementationb
 
 newtype GameStateOp s a = GameStateOp {runState :: s -> (Either String a, s)}
 
@@ -19,32 +19,47 @@ testing = do
 -- runState (pure 5 >>= \s -> pure 3) 6
 -- runState (pure 5 >> pure 3) 6
 
-instance (Show a, Show b) => Show (Status a b) where
-  show (Correct a) = show a
-  show (Wrong b) = show b
-
 instance Functor (GameStateOp st) where
-  fmap f (GameStateOp state) = GameStateOp $ \s -> 
-    let (x, newState) = state s 
-    in  (fmap f x, newState)  
+  fmap f (GameStateOp state) = GameStateOp $ \s ->
+    let (x, newState) = state s
+    in  (fmap f x, newState)
 
 instance Applicative (GameStateOp st) where
   pure x = GameStateOp $ \s -> (Right x, s)
-  (GameStateOp fstate) <*> (GameStateOp xstate) = GameStateOp $ \s -> 
+  (GameStateOp fstate) <*> (GameStateOp xstate) = GameStateOp $ \s ->
     let (f, fstate') = fstate s
         (x, xstate') = xstate fstate'
     in  (f <*> x, xstate')
 
 instance Monad (GameStateOp st) where
   return = pure
-  (GameStateOp state) >>= f = GameStateOp $ \s -> 
-    let (x, state') = state s  
+  (GameStateOp state) >>= ffake = GameStateOp $ \s ->
+    let (x, state') = state s
     in case x of Left err -> (Left err, state')
-                 Right a -> runState (f a) state'
+                 Right a -> runState (ffake a) state'
 
 -- runState SomeState - returns function part
--- lol :: Status (Board Piece) String -> Int
--- lol (Correct board) = 5
+-- Uzima state i dodaje mu na primner stack [3, 4, 5] u \s ->, funkcija f je \_ -> DrugiState(pop) tako da kada se prosledi njemu "a" (tj Either) on zapravo vraca skroz drugi state(pop) i na njemu primenjuje state'(drugi deo tuple od push 3)
+-- push 3 >>= \_ -> pop 
+board1 = Board [[Empty, Empty, Yellow, Red], [Empty, Yellow, Yellow, Yellow]]
+
+-- TODO it should be red in 63 but current player
+applyMove :: Int -> GameStateOp (Board Piece) Piece
+applyMove column = GameStateOp $ \board@(Board columns) ->
+  case not (checkIfMovesLeft board) || checkWinCon board of
+    True -> (Left "Game already finish", board)
+    False -> 
+      case column < 0 || length columns <= column of 
+        True -> (Left "Out of bounds", board)
+        False -> 
+          case newBoard == board of
+            True -> (Left "Column is already full", board)
+            False -> (Right Red, newBoard)
+          where newBoard = movePlayed board column Red
+
+applyMoves :: GameStateOp (Board Piece) Piece
+applyMoves = do
+  applyMove 1
 
 switchTurn :: Turn -> Turn
 switchTurn Player1 = Player2
